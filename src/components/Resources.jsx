6ec063e4cda3999d5b58f3dfa8b74546142a9a5e
Bjørn Andreas <70635155+BjornAndreas@users.noreply.github.com>
2025-03-13 11:47:24 +0100

    update

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -12,8 +12,8 @@ export default function Resources({ category }) {
       <ul>
         {filterRessurser.map(ressurs => (
           <li key={ressurs.url}>
-            <h2>{ressurs.title}</h2> {/* Viser tittelen fra ressurser.js */}
-            <a href={ressurs.url} target="_blank" rel="noopener noreferrer">
+            <h2>{ressurs.title}</h2>
+            <a href={ressurs.url} target="_blank">
               {ressurs.url}
             </a>
           </li>
